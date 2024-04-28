14448c8282bc08d67554df45521edbc4ccc91d44
Pinak <dingo.in4k@gmail.com>
2024-04-28 07:51:11 -0700

    1 solution added

---
+++ palindrome_number.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        
+            // Q : https://leetcode.com/problems/palindrome-number/
+            // Solution : https://leetcode.com/problems/palindrome-number/solutions/5084181/palindrome-number-simple-solution-java-eazy-and-understandable-approach
+
+        String s = String.valueOf(x);
+        int start = 0;
+        int end = s.length()-1;
+        
+        while (start <= end) {
+
+            if (s.charAt(start) == s.charAt(end)) {
+                start+=1;
+                end-=1;
+            }
+            else
+            {
+               return false; 
+                
+            }
+            
+            
+        }
+        return true;
+
+        
+        
+    }
+}
