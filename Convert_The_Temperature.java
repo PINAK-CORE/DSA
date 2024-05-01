ab9bbaa895b3f967abd9ee4a7a260708238d5b2e
Pinak <dingo.in4k@gmail.com>
2024-04-30 23:51:44 -0800

    updated

--- Convert_The_Temperature.java
+++ Convert_The_Temperature.java
@@ -1,6 +1,8 @@
 class Solution 
 {
     public double[] convertTemperature(double celsius) {
+        // Q : https://leetcode.com/problems/convert-the-temperature/
+        //solution : https://leetcode.com/problems/convert-the-temperature/solutions/5086097/convert-the-temperature-simple-solution-java-eazy-understandable
 
         double k = celsius + 273.15;
         double f = celsius * 1.80 + 32.00;
