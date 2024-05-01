fc03e4f65bb039140a7c52e6901cceb39e349d96
Pinak <dingo.in4k@gmail.com>
2024-04-30 23:41:28 -0800

    one question and solution added

---
+++ Reversing_an_integer.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int reverse(int x) {
+        
+       
+    // Q: https://leetcode.com/problems/reverse-integer/
+    // Solution : https://leetcode.com/problems/reverse-integer/solutions/5092709/reversing-an-integer-simple-java-eazy-understandable-solution
+        int num = Math.abs(x);  
+        
+        int rev = 0;  
+        
+        while (num != 0) {
+            int ld = num % 10;  
+            
+            
+            if (rev > (Integer.MAX_VALUE - ld) / 10) {
+                return 0;  
+            }
+            rev = rev * 10 + ld;  
+            num = num / 10;  
+        }
+        
+        return (x < 0) ? (-rev) : rev;
+        
+    }
+}
