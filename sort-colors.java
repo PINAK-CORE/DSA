e4c0e1b36f6e961dd129c4171a9b51189ed9f521
PINAK TILAVAT <dingo.in4k@gmail.com>
2024-06-17 11:44:16 -0700

    Q + Sol. link Added

---
+++ sort-colors.java
@@ -0,0 +1,9 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        
+        // Q    : https://leetcode.com/problems/sort-colors/
+        // Sol. : https://leetcode.com/problems/sort-colors/solutions/5325552/sort-colors-eazy-understandable-simple-solution-java 
+        Arrays.sort(nums);
+        
+    }
+}
