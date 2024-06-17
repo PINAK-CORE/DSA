97ce0f96513eaff1d567d294b6dc9e5f82be91b9
PINAK TILAVAT <dingo.in4k@gmail.com>
2024-06-17 01:07:16 -0700

    Q + Sol. link Added

---
+++ index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,10 @@
+class Solution 
+{
+    public int strStr(String haystack, String needle)
+    {
+        // Q     : https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+        // Sol   : https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/solutions/5323879/index-of-the-first-occurrence-in-a-string-eazy-understandable-simple-solution-java
+        
+        return haystack.indexOf(needle);
+    }
+}
